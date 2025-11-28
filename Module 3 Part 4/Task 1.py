99509f044bbec9a1713b2094e2539239b5b99bc3
Martinas <gushus.martinas735@gmail.com>
2025-11-28 15:12:51 +0300

    Update

--- Module 3 Part 4/Task 1.py
+++ Module 3 Part 4/Task 1.py
@@ -1,2 +1,11 @@
-while True:
-    print("АААААААААААААААААААААААААААААААААААААААААА")
+a = int(input("Введите начало диапазона: "))
+b = int(input("Введите конец диапазона: "))
+l = 0
+for i in range(a, b):
+    for j in range(1, i+1):
+        if i % j == 0:
+            l = l + 1
+        #print(f"число i {i}, число j {j}, чсило l {l}")
+    if l == 2:
+        print(i)
+    l = 0
